da2d9e9d5d6c329d0b0bd82dc54b3a319357ac12
sipra <sipramishra085@gmail.com>
2023-07-11 16:07:26 +0530

    [Sipra]Add:Program for ATMtransaction

---
+++ ATMtransaction/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMtransaction
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Transactions transactions = new Transactions();
+            Console.WriteLine("Welcome To the AXIS ATM");
+            Console.WriteLine("Enter the ATM 4 Digit Pin");
+            string pin = Console.ReadLine();
+            //string pin_one = "3456";
+            string pattern = "^[1-9]{1}[0-9]{3,5}$";
+            if (Regex.IsMatch(pin, pattern))
+            //if(pin ==pin_one)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Enter the option");
+                    Console.WriteLine("1-Money Deposition");
+                    Console.WriteLine("2-Money WithDraw");
+                    Console.WriteLine("3-Check Balance");
+                    Console.WriteLine("4-ATM Transactions");
+                    Console.WriteLine("*****************");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            transactions.Desposit();
+                            Console.WriteLine("*******************");
+                            break;
+                        case 2:
+                            transactions.Withdrawal();
+                            Console.WriteLine("********************");
+                            break;
+                        case 3:
+                            transactions.CheckBalance();
+                            Console.WriteLine("********************");
+                            break;
+                        case 4:
+                            Transation transation = new Transation();
+                            transation.Money();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option");
+                            break;
+
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid ATM pin");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ATMtransaction/Transaction.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMtransaction
+{
+    public class Transaction
+    {
+        public void Money()
+        {
+            int cash = 5000, deposite, balance;
+            while (true)
+            {
+                Console.WriteLine("Enter the option");
+                Console.WriteLine("1-Money Deposition");
+                Console.WriteLine("2-Money WithDraw");
+                Console.WriteLine("3-Check balance");
+                Console.WriteLine("4-Exit");
+                Console.WriteLine("*****************");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the amount that want to deposite");
+                        deposite = Convert.ToInt32(Console.ReadLine());
+                        cash = deposite + cash;
+                        Console.WriteLine("The Remaining balance is " + cash);
+                        Console.WriteLine("*****************");
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the amount that want to withdraw");
+                        int amount = Convert.ToInt32(Console.ReadLine());
+                        if (amount % 100 != 0)
+                        {
+                            Console.WriteLine("Withdraw amount should be multiple of 100");
+                        }
+                        else if (cash < amount)
+                        {
+                            Console.WriteLine("The account has a insufficient amount");
+                        }
+                        else if (cash <= amount)
+                        {
+                            Console.WriteLine("The account should have a minimum balance");
+                        }
+
+                        else
+                        {
+                            cash = cash - amount;
+                            Console.WriteLine("The Money Withdrawal successfully");
+                            Console.WriteLine("The Remaining balance is " + cash);
+
+                        }
+                        Console.WriteLine("*****************");
+                        break;
+                    case 3:
+                        Console.WriteLine("The total balance is " + cash);
+                        Console.WriteLine("*****************");
+                        break;
+                    case 4:
+                        Console.WriteLine("Thank You Visit Again");
+                        Console.WriteLine("*****************");
+                        break;
+                }
+            }
+        }
+    }
+}
